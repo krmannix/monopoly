ccb9c7a883cfcc91f9be9d53b6740430ff4fb6b0
K. Rodman Mannix <kevinrmannix@gmail.com>
2021-06-16 08:43:45 -0400

    GameController includes Space#space_at result in response

--- lib/monopoly_web/controllers/game_controller.ex
+++ lib/monopoly_web/controllers/game_controller.ex
@@ -5,11 +5,14 @@ defmodule MonopolyWeb.GameController do
   def roll(conn, _params) do
     dicerolls = Dice.roll_times(2)
     total = Enum.sum(dicerolls)
+    starting_index = 0
+    space = Space.space_at(starting_index, total)
     body = %{
       roll: %{
         dicerolls: dicerolls,
         total: total,
       },
+      space: space,
     }
     json(
       conn,

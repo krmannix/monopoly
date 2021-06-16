a421f8602b412e30c189ad8d004730da5387e906
K. Rodman Mannix <kevinrmannix@gmail.com>
2021-06-16 08:42:39 -0400

    Space#space_at

---
+++ lib/monopoly/space.ex
@@ -0,0 +1,11 @@
+defmodule Space do
+  @moduledoc "Models each tile on the gameboard that a user can land on"
+
+  @space_json Util.load_json_from_file!("priv/data/spaces.json")
+
+  def space_at(start_index, number_of_steps) do
+    Enum.at(spaces(), rem(start_index + number_of_steps, Enum.count(spaces())))
+  end
+
+  defp spaces, do: @space_json["spaces"]
+end

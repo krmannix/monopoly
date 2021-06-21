ab4e33e224ab9a08e87305d525b051b0c6dfaf8c
K. Rodman Mannix <kevinrmannix@gmail.com>
2021-06-21 17:19:41 -0600

    GameController#create

--- lib/monopoly_web/controllers/game_controller.ex
+++ lib/monopoly_web/controllers/game_controller.ex
@@ -2,6 +2,22 @@ defmodule MonopolyWeb.GameController do
   use MonopolyWeb, :controller
   alias Monopoly.Dice
 
+  @default_player_name "Player 1"
+
+  def create(conn, params) do
+    name = extract_name(params["name"])
+
+    body = %{
+      players: [
+        %{
+          name: name,
+        }
+      ],
+    }
+
+    json(conn, body)
+  end
+
   def roll(conn, _params) do
     dicerolls = Dice.roll_times(2)
     total = Enum.sum(dicerolls)
@@ -19,4 +35,9 @@ defmodule MonopolyWeb.GameController do
       body
     )
   end
+
+  defp extract_name(name) when is_binary(name) do
+    if String.match?(name, ~r/^[[:space:]]*$/), do: @default_player_name, else: String.trim(name)
+  end
+  defp extract_name(_), do: @default_player_name
 end

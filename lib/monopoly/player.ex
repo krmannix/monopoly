90b13890e7b59da5465785d1f10ebf9849ef7439
K. Rodman Mannix <kevinrmannix@gmail.com>
2021-06-24 18:01:21 -0600

    Player struct

---
+++ lib/monopoly/player.ex
@@ -0,0 +1,9 @@
+defmodule Player do
+  defstruct id: nil,
+            name: "Default Player",
+            money: 1500,
+            isBankrupt: false,
+            isHumanPlayer: false,
+            properties: [],
+            getOutOfJailFreeCardCount: 0
+end

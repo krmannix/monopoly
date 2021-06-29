fa4abfa46ea06f7b3906a56bdb67b09d63b0f35f
K. Rodman Mannix <kevinrmannix@gmail.com>
2021-06-29 13:31:47 -0600

    Space.spaces() generates Space structs

--- lib/monopoly/space.ex
+++ lib/monopoly/space.ex
@@ -3,11 +3,18 @@ defmodule Space do
 
   @space_json Util.load_json_from_file!("priv/data/spaces.json")
 
+  @derive Jason.Encoder
   defstruct [:id, :name, :type]
 
   def space_at(start_index, number_of_steps) do
     Enum.at(spaces(), rem(start_index + number_of_steps, Enum.count(spaces())))
   end
 
-  defp spaces, do: @space_json["spaces"]
+  defp spaces do
+    Enum.map(@space_json["spaces"], fn(space) ->
+      space_with_atom_keys = Enum.map(space, fn ({key, value}) -> {String.to_atom(key), value} end)
+
+      struct(Space, space_with_atom_keys)
+    end)
+  end
 end
--- test/monopoly/space_test.exs
+++ test/monopoly/space_test.exs
@@ -7,60 +7,60 @@ defmodule Monopoly.SpaceTest do
   describe "#space_at" do
     test "it handles starting at 0 and rolling less than the number of spaces" do
       result = Space.space_at(@first_index, 4)
-      expected = %{
-        "id" => "8afe3723-4439-4a75-b99e-3e4f0f5646aa",
-        "name" => "Income Tax",
-        "type" => "property",
+      expected = %Space{
+        id: "8afe3723-4439-4a75-b99e-3e4f0f5646aa",
+        name: "Income Tax",
+        type: "property",
       }
       assert result == expected
     end
 
     test "it handles starting at 0 and rolling more than the number of spaces" do
       result = Space.space_at(@first_index, 42)
-      expected = %{
-        "id" => "6cba37bd-a6ac-499e-b701-58333790e996",
-        "name" => "Community Chest",
-        "type" => "community_chest",
+      expected = %Space{
+        id: "6cba37bd-a6ac-499e-b701-58333790e996",
+        name: "Community Chest",
+        type: "community_chest",
       }
       assert result == expected
     end
 
     test "it handles starting at 0 and rolling the number of spaces" do
       result = Space.space_at(@first_index, 40)
-      expected = %{
-        "id" => "8c826137-989e-4b3c-bbeb-ae2aa83930b7",
-        "name" => "Go",
-        "type" => "go",
+      expected = %Space{
+        id: "8c826137-989e-4b3c-bbeb-ae2aa83930b7",
+        name: "Go",
+        type: "go",
       }
       assert result == expected
     end
 
     test "it handles starting at the last index and rolling less than the number of spaces" do
       result = Space.space_at(@last_index, 4)
-      expected = %{
-        "id" => "6d48a337-3ad0-4283-a9a1-42f161300460",
-        "name" => "Baltic Avenue",
-        "type" => "property",
+      expected = %Space{
+        id: "6d48a337-3ad0-4283-a9a1-42f161300460",
+        name: "Baltic Avenue",
+        type: "property",
       }
       assert result == expected
     end
 
     test "it handles starting at the last index and rolling more than the number of spaces" do
       result = Space.space_at(@last_index, 42)
-      expected = %{
-        "id" => "ae5ce454-9aeb-4660-a318-cc635def636d",
-        "name" => "Mediterranean Avenue",
-        "type" => "property",
+      expected = %Space{
+        id: "ae5ce454-9aeb-4660-a318-cc635def636d",
+        name: "Mediterranean Avenue",
+        type: "property",
       }
       assert result == expected
     end
 
     test "it handles starting at the last index and rolling the number of spaces" do
       result = Space.space_at(@last_index, 40)
-      expected = %{
-        "id" => "256b3b66-60d1-4796-817e-84a3f7b36b23",
-        "name" => "Boardwalk",
-        "type" => "property",
+      expected = %Space{
+        id: "256b3b66-60d1-4796-817e-84a3f7b36b23",
+        name: "Boardwalk",
+        type: "property",
       }
       assert result == expected
     end

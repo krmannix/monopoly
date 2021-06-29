246d80f593f5de7dafdfa3caea045b3c161dc93e
K. Rodman Mannix <kevinrmannix@gmail.com>
2021-06-29 13:57:45 -0600

    add currentSpace to Player (#7)
    * add id and type to spaces.json
    
    * Space defstruct :id, :name, :type
    
    * Space.spaces() generates Space structs
    
    * Space.starting_space
    
    * Player.currentSpace

--- lib/monopoly/space.ex
+++ lib/monopoly/space.ex
@@ -2,10 +2,22 @@ defmodule Space do
   @moduledoc "Models each tile on the gameboard that a user can land on"
 
   @space_json Util.load_json_from_file!("priv/data/spaces.json")
+  @go_type "go"
+
+  @derive Jason.Encoder
+  defstruct [:id, :name, :type]
 
   def space_at(start_index, number_of_steps) do
     Enum.at(spaces(), rem(start_index + number_of_steps, Enum.count(spaces())))
   end
 
-  defp spaces, do: @space_json["spaces"]
+  def starting_space, do: Enum.find(spaces(), fn (space) -> space.type == @go_type end)
+
+  defp spaces do
+    Enum.map(@space_json["spaces"], fn(space) ->
+      space_with_atom_keys = Enum.map(space, fn ({key, value}) -> {String.to_atom(key), value} end)
+
+      struct(Space, space_with_atom_keys)
+    end)
+  end
 end

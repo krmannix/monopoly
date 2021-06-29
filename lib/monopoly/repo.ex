4d129ce028c766d94022e31fcff7cf0443e9a3a3
K. Rodman Mannix <kevinrmannix@gmail.com>
2021-06-29 14:11:14 -0600

    Monopoly.Schema

--- lib/monopoly/repo.ex
+++ lib/monopoly/repo.ex
@@ -1,5 +1,6 @@
 defmodule Monopoly.Repo do
   use Ecto.Repo,
     otp_app: :monopoly,
-    adapter: Ecto.Adapters.Postgres
+    adapter: Ecto.Adapters.Postgres,
+    migration_primary_key: [name: :id, type: :binary_id]
 end

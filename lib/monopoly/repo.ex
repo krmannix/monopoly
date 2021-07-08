61df1fa66693abd9671b7b5e1c2d49997cdcbc2e
K. Rodman Mannix <kevinrmannix@gmail.com>
2021-07-08 08:21:10 -0400

    undo unused Repo changes

--- lib/monopoly/repo.ex
+++ lib/monopoly/repo.ex
@@ -1,6 +1,5 @@
 defmodule Monopoly.Repo do
   use Ecto.Repo,
     otp_app: :monopoly,
-    adapter: Ecto.Adapters.Postgres,
-    migration_primary_key: [name: :id, type: :binary_id]
+    adapter: Ecto.Adapters.Postgres
 end
